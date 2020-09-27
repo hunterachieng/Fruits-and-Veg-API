311be604ae6a07e4a98211ecf8d22fc964d92703
gerysantoso03 <Gerysantos03@gmail.com>
2020-09-27 22:14:30 +0700

    Added login and logout method

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -31,3 +31,56 @@ exports.register = async (req, res, next) => {
         return next(err);
     }
 };
+
+exports.login = async (req, res, next) => {
+    try {
+        if (req.cookies.user_sid) {
+            return res.json({
+                status: 'failed',
+                message: 'You are already logged in !!',
+            });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.send('There is no user exists');
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password);
+
+        if (!comparePassword) {
+            return res.status(400).send('Password are invalids');
+        } else {
+            req.session.user = user._id;
+            res.status(200).json({
+                status: 'success',
+                message: 'Your login was successfull :)',
+            });
+        }
+    } catch (err) {
+        return next(err);
+    }
+};
+
+exports.logout = async (req, res, next) => {
+    try {
+        if (req.session.user && req.cookies.user_sid) {
+            /* Unsafe method, maybe later this method will change for security interest */
+            res.clearCookie('user_sid');
+
+            res.status(200).json({
+                status: 'success',
+            });
+        } else {
+            res.status(400).json({
+                status: 'failed',
+                message: 'Logout failed',
+            });
+        }
+    } catch (err) {
+        return next(err);
+    }
+};
