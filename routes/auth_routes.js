0865ce815777f9525a843c8ad4b741b63a384693
gerysantoso03 <Gerysantos03@gmail.com>
2020-09-27 22:14:50 +0700

    Added login and logout route

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -6,4 +6,8 @@ const authControllers = require('../controllers/auth_controllers');
 
 router.route('/register').post(authControllers.register);
 
+router.route('/login').post(authControllers.login);
+
+router.route('/logout').get(authControllers.logout);
+
 module.exports = router;
